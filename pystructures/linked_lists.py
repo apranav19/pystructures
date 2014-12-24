ac67bbd3a7f4e1de564411636c4ac584c7f2cabd
PranavAngara19 <apranav19@gmail.com>
2014-12-24 00:05:15 -0800

    Add Abstract Linked List

--- pystructures/linked_lists.py
+++ pystructures/linked_lists.py
@@ -3,7 +3,9 @@
 	- Singly Linked Lists
 	- Doubly Linked Lists
 """
+from future.builtins import with_metaclass
 from builtins import object, str
+from abc import ABCMeta, abstractmethod
 
 class Node(object):
     """
@@ -40,3 +42,41 @@ class Node(object):
 
     def __str__(self):
         return str(self.__value)
+
+class AbstractList(with_metaclass(ABCMeta, object)):
+    """ An abstract definition of an unordered list """
+
+    def __init__(self):
+        self.__head = None
+        self.__size = 0
+
+    @abstractmethod
+    def insert(self, item):
+        """ Appends an item to the list """
+        pass
+
+    @abstractmethod
+    def remove(self, item):
+        """ Remove an item from the list """
+
+    @property
+    def size(self):
+        """ Returns the size of the list """
+        return self.__size
+    
+    def is_empty(self):
+        """ Returns true if size is 0 false otherwise """
+        return self.__size == 0
+
+    def contains(self, item):
+        """ Search for an item in the list """
+        res, current = False, self.__head
+
+        while current != None:
+            if current.value == item:
+                res = True
+                break
+            current = current.next
+
+        return res
+        
