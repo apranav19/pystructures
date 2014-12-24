4428ece32c5893ba3eaac749ec5c7c2769c3fc30
PranavAngara19 <apranav19@gmail.com>
2014-12-23 23:39:47 -0800

    Add additional Node test case

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,8 +1,15 @@
 from unittest import TestCase
 from pystructures.linked_lists import Node
 
+
 class TestNode(TestCase):
-	def test_value(self):
-		""" A simple test to check the Node's value """
-		node = Node(10)
-		self.assertEqual(10, node.value)
+    def test_value(self):
+        """ A simple test to check the Node's value """
+        node = Node(10)
+        self.assertEqual(10, node.value)
+
+    def test_improper_node(self):
+        """ A test to check if an invalid data type is set as a node's next"""
+        node = Node(10)
+        with self.assertRaises(ValueError):
+            node.next = "Hello"
