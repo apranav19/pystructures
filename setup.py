3061be5fe8c9761a8d750dbeccefbe6ae7915e90
PranavAngara19 <apranav19@gmail.com>
2014-12-27 21:53:01 -0800

    Reduce requirements load

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pystructures',
-      version='0.0.1',
+      version='0.0.2',
       description='Data Structures made simple',
       url='http://github.com/apranav19/pystructures',
       author='Pranav Angara',
